11759e7a2b36bf91a207daf2f3b5c12e386c295e
Cutreson <dinhhongsontbpt@gmail.com>
2022-10-11 10:09:24 +0700

    ok

---
+++ SLMPProtocol/Command.cs
@@ -0,0 +1,12 @@
+﻿namespace SlmpProtocol
+{
+    /// <summary>
+    /// This enum encodes the supported SLMP commands within this library.
+    /// </summary>
+    public enum Command
+    {
+        DeviceRead = 0x0401,
+        DeviceWrite = 0x1401,
+        SelfTest = 0x0619,
+    }
+}
